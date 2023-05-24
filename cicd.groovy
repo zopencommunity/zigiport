4ee27ebf6ee2bdc70071141d88f0c70ae807d3eb
Igor Todorovski <itodorov@ca.ibm.com>
2023-05-24 13:29:08 -0400

    add polling to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,5 +1,14 @@
 node('linux')
 {
+   
+  stage (Poll) {
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/zigiport.git"]]])
+        }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/zigiport.git'), string(name: 'PORT_DESCRIPTION', value: 'z/OS ISPF Git interface' )]
   }
