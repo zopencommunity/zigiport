01af7198ea91930e7f5f9248aa588a4b5b886af0
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:21:21 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/zigiport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/zigiport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/zigiport.git'), string(name: 'PORT_DESCRIPTION', value: 'z/OS ISPF Git interface' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/zigiport.git'), string(name: 'PORT_DESCRIPTION', value: 'z/OS ISPF Git interface' )]
   }
 }
